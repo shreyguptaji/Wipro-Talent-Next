0b094c5a2400562c707ec4151dbb004a189b3668
shreyguptaji <31301609+shreyguptaji@users.noreply.github.com>
2018-09-30 18:36:25 +0530

    Add files via upload

---
+++ 1. language basic/project1.java
@@ -0,0 +1,15 @@
+class project1
+{ 
+ public static void main (String args[])
+{
+  
+   String s1  = args[0]; 
+   String s2 = args[1];
+
+  System.out.print(s1+"  " + "technologies" + "  "  +s2);
+  
+   
+}
+}
+
+    
---
+++ 1. language basic/project2.java
@@ -0,0 +1,13 @@
+class project2
+{ 
+ public static void main (String args[])
+{
+  
+   String s1  = args[0]; 
+  System.out.print("Welcome" + "  "  +s1);
+  
+   
+}
+}
+
+    
